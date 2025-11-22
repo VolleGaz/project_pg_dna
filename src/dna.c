7e8eb8bc5fa207e32109e41257fbf0a7deeb5a0a
David Amsens <david.amsens@ulb.be>
2025-11-22 16:32:28 +0100

    add error handler for dna

--- src/dna.c
+++ src/dna.c
@@ -3,88 +3,225 @@
 #include "fmgr.h"
 #include "utils/varlena.h"
 #include "utils/builtins.h"
+#include "utils/memutils.h"
 #include "dna.h"
 
 #include <ctype.h>
 #include <string.h>
 
+/*
+ * IMPORTANT:
+ * PG_MODULE_MAGIC is defined in pg_dna.c, not here.
+ */
 
 PG_FUNCTION_INFO_V1(dna_in);
 PG_FUNCTION_INFO_V1(dna_out);
 PG_FUNCTION_INFO_V1(dna_length);
+PG_FUNCTION_INFO_V1(dna_get);
 
-/* Encode base to 2 bits */
+/* -------------------------------------------------------------------------
+ * Helpers
+ * ------------------------------------------------------------------------- */
+
+/*
+ * Encode one base (char) into 2 bits (0..3).
+ * Accepts both upper- and lowercase A/C/G/T.
+ * Raises a PostgreSQL ERROR on invalid character.
+ */
 static unsigned char
 encode_base(char c)
 {
-    switch (c)
+    unsigned char uc;
+
+    uc = (unsigned char) c;
+    uc = (unsigned char) toupper(uc);
+
+    switch (uc)
     {
-        case 'A': case 'a': return 0;
-        case 'C': case 'c': return 1;
-        case 'G': case 'g': return 2;
-        case 'T': case 't': return 3;
+        case 'A':
+            return 0;
+        case 'C':
+            return 1;
+        case 'G':
+            return 2;
+        case 'T':
+            return 3;
         default:
             ereport(ERROR,
                     (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
                      errmsg("invalid DNA base: '%c' (allowed: A,C,G,T only)", c)));
+            /* not reached */
             return 0;
     }
 }
 
-/* Decode 2 bits to base */
+/*
+ * Decode 2 bits (0..3) back into a base character.
+ * This should never see values outside 0..3 if the data is consistent.
+ */
 static char
 decode_base(unsigned char b)
 {
     static const char map[4] = { 'A', 'C', 'G', 'T' };
-    return map[b & 0x3];
+
+    b &= 0x03;
+    return map[b];
 }
 
-/* Input function */
+/*
+ * Check that the internal varlena value is consistent:
+ *  - size is large enough to contain header + length + packed data
+ *  - length is not absurdly large
+ * Raises ERROR if something looks corrupted (e.g. malformed toast value).
+ */
+static void
+check_dna_consistency(const Dna *dna)
+{
+    Size size;
+    uint32 n;
+    uint32 packed_bytes;
+    Size min_size;
+
+    size = VARSIZE_ANY(dna);    /* full varlena size */
+    n = dna->length;
+
+    if (n > DNA_MAX_LENGTH)
+    {
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_CORRUPTED),
+                 errmsg("dna value has unreasonable length: %u", n)));
+    }
+
+    packed_bytes = DNA_PACKED_BYTES(n);
+    min_size = offsetof(Dna, data) + packed_bytes;
+
+    if (size < min_size)
+    {
+        ereport(ERROR,
+                (errcode(ERRCODE_DATA_CORRUPTED),
+                 errmsg("dna value is corrupted: size %zu too small for length %u",
+                        (size_t) size, n)));
+    }
+}
+
+/* -------------------------------------------------------------------------
+ * Input function: dna_in(cstring) -> dna
+ * ------------------------------------------------------------------------- */
+
 Datum
 dna_in(PG_FUNCTION_ARGS)
 {
-    char   *input = PG_GETARG_CSTRING(0);
-    uint32  n     = (uint32) strlen(input);
-    uint32  packed_bytes = DNA_PACKED_BYTES(n);
+    char   *input;
+    size_t  input_len;
+    uint32  n;
+    uint32  packed_bytes;
+    Size    size;
+    Dna    *result;
+    uint32  i;
 
-    /* Correct varlen allocation */
-    Size size = offsetof(Dna, data) + packed_bytes;
+    input = PG_GETARG_CSTRING(0);
+    input_len = strlen(input);
 
-    Dna *result = (Dna *) palloc(size);
+    if (input_len > DNA_MAX_LENGTH)
+    {
+        ereport(ERROR,
+                (errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
+                 errmsg("DNA sequence too long (%zu bases, max is %u)",
+                        input_len, DNA_MAX_LENGTH)));
+    }
 
-    SET_VARSIZE(result, size);   /* sets vl_len_ */
+    n = (uint32) input_len;
+    packed_bytes = DNA_PACKED_BYTES(n);
+
+    /*
+     * Compute total size of the varlena structure.
+     * We use offsetof(Dna, data) to get the offset of the data[] field,
+     * then add the number of packed bytes.
+     */
+    size = offsetof(Dna, data) + packed_bytes;
+
+    if (size > MaxAllocSize)
+    {
+        ereport(ERROR,
+                (errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
+                 errmsg("DNA value size exceeds maximum allocatable size")));
+    }
+
+    result = (Dna *) palloc(size);
+
+    /* Set varlena header */
+    SET_VARSIZE(result, size);
+
+    /* Store logical length in bases */
     result->length = n;
 
-    memset(result->data, 0, packed_bytes);
+    /* Initialize packed bytes to zero */
+    if (packed_bytes > 0)
+        memset(result->data, 0, packed_bytes);
 
-    for (uint32 i = 0; i < n; i++)
+    /*
+     * Pack bases: 4 bases per byte, big-endian inside each byte:
+     *   bits 7..6 => base 0
+     *   bits 5..4 => base 1
+     *   bits 3..2 => base 2
+     *   bits 1..0 => base 3
+     */
+    for (i = 0; i < n; i++)
     {
-        unsigned char b = encode_base(input[i]);
+        unsigned char b;
+        uint32        byte_index;
+        uint32        shift;
 
-        uint32 byte_index = i / 4;
-        uint32 shift      = (3 - (i % 4)) * 2;
+        b = encode_base(input[i]);
 
-        result->data[byte_index] |= (b << shift);
+        byte_index = i / 4;
+        shift      = (3 - (i % 4)) * 2;
+
+        result->data[byte_index] |= (unsigned char) (b << shift);
     }
 
     PG_RETURN_POINTER(result);
 }
 
-/* Output function */
+/* -------------------------------------------------------------------------
+ * Output function: dna_out(dna) -> cstring
+ * ------------------------------------------------------------------------- */
+
 Datum
 dna_out(PG_FUNCTION_ARGS)
 {
-    Dna    *dna = (Dna *) PG_GETARG_POINTER(0);
-    uint32  n   = dna->length;
+    Datum  arg;
+    Dna   *dna;
+    uint32 n;
+    uint32 i;
+    char  *buf;
 
-    char *buf = (char *) palloc(n + 1);
+    /*
+     * Obtain a de-toasted, packed representation of the varlena value.
+     * The returned pointer may point to a copied value that we own, or to
+     * a read-only buffer; we must not pfree(dna).
+     */
+    arg = PG_GETARG_DATUM(0);
+    dna = (Dna *) PG_DETOAST_DATUM_PACKED(arg);
 
-    for (uint32 i = 0; i < n; i++)
+    /* Sanity check on internal structure. */
+    check_dna_consistency(dna);
+
+    n = dna->length;
+
+    buf = (char *) palloc(n + 1);
+
+    for (i = 0; i < n; i++)
     {
-        uint32 byte_index = i / 4;
-        uint32 shift      = (3 - (i % 4)) * 2;
+        uint32 byte_index;
+        uint32 shift;
+        unsigned char packed;
 
-        buf[i] = decode_base((dna->data[byte_index] >> shift) & 0x3);
+        byte_index = i / 4;
+        shift      = (3 - (i % 4)) * 2;
+
+        packed = (unsigned char) ((dna->data[byte_index] >> shift) & 0x03);
+        buf[i] = decode_base(packed);
     }
 
     buf[n] = '\0';
@@ -92,53 +229,68 @@ dna_out(PG_FUNCTION_ARGS)
     PG_RETURN_CSTRING(buf);
 }
 
-/* Length function */
+/* -------------------------------------------------------------------------
+ * dna_length(dna) -> integer
+ * ------------------------------------------------------------------------- */
+
 Datum
 dna_length(PG_FUNCTION_ARGS)
 {
-    Dna *dna = (Dna *) PG_GETARG_POINTER(0);
+    Datum  arg;
+    Dna   *dna;
+
+    arg = PG_GETARG_DATUM(0);
+    dna = (Dna *) PG_DETOAST_DATUM_PACKED(arg);
+
+    check_dna_consistency(dna);
+
     PG_RETURN_INT32((int32) dna->length);
 }
 
-PG_FUNCTION_INFO_V1(dna_get);
+/* -------------------------------------------------------------------------
+ * dna_get(dna, integer) -> text (single character)
+ * ------------------------------------------------------------------------- */
 
-/*
- * dna_get(dna, integer) -> text (single char)
- *
- * Returns the i-th base of the DNA sequence (1-based indexing).
- */
 Datum
 dna_get(PG_FUNCTION_ARGS)
 {
-    Dna *dna = (Dna *) PG_GETARG_POINTER(0);
-    int32 index = PG_GETARG_INT32(1);
+    Datum  arg;
+    Dna   *dna;
+    int32  idx;
+    uint32 n;
+    uint32 i;
+    uint32 byte_index;
+    uint32 shift;
+    unsigned char packed;
+    char   ch;
+    text  *result_text;
 
-    if (index < 1 || index > dna->length)
-        ereport(ERROR,
-                (errcode(ERRCODE_ARRAY_SUBSCRIPT_ERROR),
-                 errmsg("index %d out of range (1..%u)", index, dna->length)));
+    arg = PG_GETARG_DATUM(0);
+    dna = (Dna *) PG_DETOAST_DATUM_PACKED(arg);
 
-    /* convert to 0-based */
-    uint32 i = index - 1;
+    check_dna_consistency(dna);
 
-    uint32 byte_index = i / 4;
-    uint32 shift = (3 - (i % 4)) * 2;
+    idx = PG_GETARG_INT32(1);
+    n   = dna->length;
 
-    unsigned char packed = (dna->data[byte_index] >> shift) & 0x3;
-
-    char result_char;
-    switch (packed)
+    if (idx < 1 || (uint32) idx > n)
     {
-        case 0: result_char = 'A'; break;
-        case 1: result_char = 'C'; break;
-        case 2: result_char = 'G'; break;
-        case 3: result_char = 'T'; break;
-        default: result_char = '?'; /* impossible */
+        ereport(ERROR,
+                (errcode(ERRCODE_ARRAY_ELEMENT_ERROR),
+                 errmsg("dna_get: index %d out of bounds (valid range: 1..%u)",
+                        idx, n)));
     }
 
-    /* return a text of length 1 */
-    text *out = cstring_to_text_with_len(&result_char, 1);
+    /* Convert to 0-based index */
+    i = (uint32) (idx - 1);
 
-    PG_RETURN_TEXT_P(out);
+    byte_index = i / 4;
+    shift      = (3 - (i % 4)) * 2;
+
+    packed = (unsigned char) ((dna->data[byte_index] >> shift) & 0x03);
+    ch = decode_base(packed);
+
+    result_text = cstring_to_text_with_len(&ch, 1);
+
+    PG_RETURN_TEXT_P(result_text);
 }
-
--- src/dna.h
+++ src/dna.h
@@ -6,19 +6,31 @@
 /*
  * Internal representation of the dna type (varlena).
  *
- * Layout:
- *   [ int32 vl_len_ ]   -- varlena header
- *   [ uint32 length ]   -- number of bases
- *   [ unsigned char data[] ] -- packed bases
+ * Memory layout:
+ *
+ *   [ int32  vl_len_ ]   -- varlena header
+ *   [ uint32 length ]    -- logical number of bases
+ *   [ unsigned char data[] ] -- packed bases (2 bits/base)
+ *
+ * This struct MUST match what we allocate in dna_in().
  */
 typedef struct Dna
 {
-    int32         vl_len_;   /* varlena header */
+    int32         vl_len_;   /* varlena header (do not touch directly) */
     uint32        length;    /* number of bases */
     unsigned char data[FLEXIBLE_ARRAY_MEMBER]; /* packed bases */
 } Dna;
 
-/* 4 bases par octet => (n + 3) / 4 */
+/*
+ * Number of bytes needed to store n bases with 2 bits/base.
+ * 4 bases per byte => ceil(n / 4) = (n + 3) / 4.
+ */
 #define DNA_PACKED_BYTES(n)   (((n) + 3) / 4)
+
+/*
+ * Hard limit to avoid absurdly large allocations.
+ * You can adjust this value depending on project constraints.
+ */
+#define DNA_MAX_LENGTH   (100000000U)  /* 1e8 bases */
 
 #endif /* PG_DNA_DNA_H */
